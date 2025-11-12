66a010172cb748a47c83b68d90a316c2d471ecb3
VuongDuc <gachhoangthang41@gmail.com>
2025-11-12 14:23:10 +0700

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    // Không chặn build vì lỗi ESLint (vẫn chạy lint local được)
+    ignoreDuringBuilds: true,
+  },
+    typescript: {
+    // Giữ nguyên kiểm tra TypeScript (an toàn). Nếu bạn cần vượt qua mọi lỗi TS (không khuyến nghị) thì đặt true.
+    ignoreBuildErrors: false,
+  },
   images: {
     domains: [
       "lh3.googleusercontent.com",   
